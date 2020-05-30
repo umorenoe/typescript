a8863a34ba66da83fce787ada03ac0f9a0050d68
Uriel Moreno <umoreno@gmail.com>
2020-05-29 23:07:02 -0500

    crear funcion

--- holamundo.js
+++ holamundo.js
@@ -1,3 +1,8 @@
 var holamundo;
 holamundo = "Hola Mundo";
 console.log(holamundo);
+function holaMundoFuncion() {
+    return "Hola mundo en funcion";
+}
+holamundo = holaMundoFuncion();
+console.log(holamundo);
--- holamundo.ts
+++ holamundo.ts
@@ -1,4 +1,10 @@
 let holamundo:string;
 holamundo = "Hola Mundo";
+console.log(holamundo);
 
+function holaMundoFuncion():string {
+  return "Hola mundo en funcion";
+}
+
+holamundo = holaMundoFuncion();
 console.log(holamundo);
