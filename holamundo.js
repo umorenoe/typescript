2550d537f9bb1a9886e691eea54dd1177a77ee44
Uriel Moreno <umoreno@gmail.com>
2020-05-30 12:58:28 -0500

    interface

--- holamundo.js
+++ holamundo.js
@@ -6,3 +6,10 @@ function holaMundoFuncion() {
 }
 holamundo = holaMundoFuncion();
 console.log(holamundo);
+function createSquare(configSquare) {
+    var color = configSquare.color;
+    var area = configSquare.width * configSquare.width;
+    return { color: color, area: area };
+}
+var square = createSquare({ color: "Green", width: 100 });
+console.log(square.area.toString());
--- holamundo.ts
+++ holamundo.ts
@@ -1,10 +1,24 @@
-let holamundo:string;
+let holamundo: string;
 holamundo = "Hola Mundo";
 console.log(holamundo);
 
-function holaMundoFuncion():string {
+function holaMundoFuncion(): string {
   return "Hola mundo en funcion";
 }
 
 holamundo = holaMundoFuncion();
 console.log(holamundo);
+
+interface SquareConfig {
+  color?: string;
+  width?: number;
+}
+
+function createSquare(configSquare:SquareConfig): {color: string, area: number} {
+  let color: string = configSquare.color;
+  let area: number = configSquare.width * configSquare.width;
+  return {color: color, area: area};
+}
+
+let square = createSquare({color: "Green", width: 100});
+console.log(square.area.toString());
